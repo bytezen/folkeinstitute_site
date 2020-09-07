05bcb8dea2532c361e0836b277bf526d3d5633a8
Rhazes Spell <rspell@rspell-new-gambp.local>
2020-09-07 13:31:18 -0700

    working on portfolio

---
+++ model_portfolio_from_json.js
@@ -0,0 +1,13 @@
+{
+  "data" : {
+    "type" : "jsonfile",
+    "filename" : "portfolio.js",
+    "record" : "db"
+    "items" : [
+      {"name" : "title", "nodename" : "title"},
+      {"name" : "description", "nodename" : "description"},
+      {"name" : "date", "nodename" : "date"},
+      {"name" : "link", "nodename" : "link"}
+    ]
+  }
+}
---
+++ portfolio.js
@@ -0,0 +1,97 @@
+{
+  "db" : [
+    { 	"title" : "Not Just for Parenting, Skills for Life", 
+    	"description" : "Have you ever struggled with modeling good behavior for your children? Do you know the difference between emotional and social intelligence? Self-awareness and self-improvement is tough work, so we’ve brought in The Folke Institute for Transformative Learning to host two different workshops on the subjects as a part of Family Academy: Courses and Training (FACT).", 
+    	"year" : 2019,
+    	"month": 3,
+    	"day": 1,
+    	"link": "https://www.philasd.org/face/2019/03/01/come-learn-skills-not-just-for-parenting-but-life/" ,
+    	"linkName": "Parenting Workshop",
+    	"type": "workshop"
+    },
+
+    { 	"title" : "4 Actions Towards an Assertive Approach for Women at Work", 
+    	"description" : "For women who have been socialized to be people pleasers, becoming assertive at work can seem daunting. But the rewards of being assertive are worth the effort of transformation, says Carrie Spell-Hansson, CEO of the Folke Institute for Transformative Learning and an AMA instructor.", 
+    	"year" : 2018,
+    	"month": 12,
+    	"day": 27,
+    	"link": "Actions_Toward_an_Assertive_Approach_for_Women_at_Work.pdf" ,
+    	"linkName": "Assertive Approach for Women at Work",
+    	"type": "article"
+    },
+
+    { 	"title" : "Avoiding the Cost of a Disengaged Workforce", 
+    	"description" : "Companies miss the mark when trying to achieve employee engagement without first considering inclusion. Engagement (or lack of engagement) is a critical driver in employee retention. According to the Coporate Executive Board's Corporate Leadership Council engaged employees are 87%.", 
+    	"year" : 2015,
+    	"month": 9,
+    	"link": "disengaged_workforce_article.pdf" ,
+    	"linkName": "Avoiding the Cost of a Disengaged Workforce",
+    	"type": "article"
+    },
+
+    { 	"title" : "Creating an Inclusive Culture in a Global Work Environment", 
+    	"description" : "Even as technology makes the world appear smaller, managing diversity and inclusion (D&I) remains a significant challenge for organizations. To succeed in today’s environment, they must commit to developing an inclusive culture.", 
+    	"year" : 2017,
+    	"month": 10,
+    	"day": 10,
+    	"link": "Creating_an_Inclusive_Culture_in_a_Global_Work_Environment.pdf" ,
+    	"linkName": "Creating an Inclusive Culture in a Global Work Environment",
+    	"type": "article"
+    },
+
+    { 	"title" : "Getting Ready for Tomorrow", 
+    	"description" : "We are experiencing a perfect storm of racial injustices while in the Covid-19 shutdown.  Should teams or organizations be taking on diversity and inclusion changes right now?", 
+    	"year" : 2020,
+    	"month": 7,    	
+    	"link": "https://vimeo.com/438525502" ,
+    	"linkName": "reBoot #63",
+    	"type": "video"
+    },    
+
+    { 	"title" : "Managing Diversity, Equity, and Inclusion", 
+    	"description" : "The following blog is provided by Carrie Spell-Hansson. It is a companion to her interview on Innovating Leadership, Co-creating Our Future titled Diversity Training Then and Now: What Has Changed? that aired on March 31st, 2020.   This interview was part of the 12-week series from the International Leadership Association.", 
+    	"year" : 2020,
+    	"month": 4,    	
+    	"day": 1,
+    	"link": "https://www.innovativeleadershipinstitute.com/managing-diversity-equity-and-inclusion/" ,
+    	"linkName": "Managing Diversity, Equity, and Inclusion",
+    	"type": "blog"
+    },    
+
+    { 	"title" : "Life:  It’s Just a Conversation", 
+    	"description" : "What are you thinking?  Look around you and there you have the product of your thoughts.  Experience follows thought as surely as night follows day.  And like it or not, when we are attached to thoughts, habits or norms, when things go differently from these patterns we automatically enter into the state of stress.  Change, is the prime example; we most often resist change even when it is for the better!", 
+    	"year" : 2015,
+    	"month": 2,    	
+    	"day": 15,
+    	"link": "https://www.blogtalkradio.com/sis-in-spirit/2015/02/20/life-its-just-a-conversation" ,
+    	"linkName": "Life: It's Just a Conversation",
+    	"type": "video"
+    },    
+
+    { 	"title" : "A Sense of Control", 
+    	"description" : "Why is it critical during a pandemic and social injustices for leaders to push pause, create a space to reconnect to the logical?  How does one engage and commit to take control?  What does that look like based on one’s leadership style or gender difference?", 
+    	"year" : 2020,
+    	"month": 5,    	
+    	"day": 15,
+    	"link": "http://youtube.com" ,
+    	"linkName": "YouTube Interview",
+    	"type": "video"
+    },    
+
+    { 	"title" : "Diversity Training Then and Now: What Has Changed?", 
+    	"description" : "Executive Order 9981 was issued on July 26, 1948, by President Harry S. Truman. This order abolished racial discrimination in the United States Armed Forces and led to the end of segregation in the services.  What have we learned about diversity and inclusion training in the 70 plus years since that Order?", 
+    	"year" : 2020,
+    	"month": 3,    	
+    	"day": 31,
+    	"link": "https://www.voiceamerica.com/episode/119410/diversity-training-then-and-now-what-has-changed" ,
+    	"linkName": "Innovating Leadership Interview",
+    	"type": "video"
+    }      
+
+
+  ],
+
+  "config": {
+  	"article_path" : "assets"
+  }
+}
